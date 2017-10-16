eb4b23562a7f1b456757106a0842c2f27eac2763
bdietz56 <bradleydietz@Bradleys-MacBook-Pro.local>
2017-10-16 13:24:45 -0400

    Final

---
+++ BasicGit.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.text.*;
+import java.io.*;
+public class BasicGit {
+
+
+   public static void main(String []args) {
+      for(int i=1; i<=10; i++)
+      {
+	System.out.println(i);
+      }
+   }
+}
+
